1e47db229e8757052b42416ce884aab90d692345
nightnotes <nightnotesplaylist@gmail.com>
2025-08-15 17:07:53 +0200

    Add files via upload

--- public/streams-append.js
+++ public/streams-append.js
@@ -1,24 +1,20 @@
 
 (function(){
   var extra = [{"name": "Dreamflow", "spotifyId": "3JxvfjZaLHM60yYRt7BYZm"}, {"name": "Poluz", "spotifyId": "0vaXEuhH3eaJuTdMoLFdbN"}, {"name": "Doris Lost", "spotifyId": "43U1R9AZoGI3V5iaW6lht8"}, {"name": "Eternal", "spotifyId": "4oOqA7kbwce90hbDDKjoID"}, {"name": "Slaapmutsje", "spotifyId": "1iH0DmClTXD3DEXO490gbq"}, {"name": "ZizZa", "spotifyId": "20ajFDuyJzM8xGkWL9agiV"}, {"name": "Sleepy Teas", "spotifyId": "3Ax9FlTyHNJdOhAKaxhZl9"}];
-  function dedupePush(list, item){
+  function add(list, item){
     if (!Array.isArray(list)) return;
     var exists = list.some(function(a){ return a?.spotifyId === item.spotifyId || a?.name === item.name; });
     if (!exists) list.push(item);
   }
-  function tryAppend(){
-    var lists = [];
-    if (typeof window !== 'undefined') {
-      lists = [window.ARTISTS, window.artists, window.StreamsArtists, window.__ARTISTS__];
-    }
-    var touched = false;
+  function inject(){
+    var lists = [window.ARTISTS, window.artists, window.StreamsArtists, window.__ARTISTS__];
     for (var i=0;i<lists.length;i++){
-      if (Array.isArray(lists[i])) {
-        for (var j=0;j<extra.length;j++){ dedupePush(lists[i], extra[j]); }
-        touched = true;
+      if (Array.isArray(lists[i])){
+        for (var j=0;j<extra.length;j++) add(lists[i], extra[j]);
+        return true;
       }
     }
-    return touched;
+    return false;
   }
-  if (!tryAppend()) window.addEventListener('load', tryAppend);
-})();
+  if (!inject()) window.addEventListener('load', inject);
+})();    

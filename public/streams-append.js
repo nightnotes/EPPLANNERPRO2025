dbc510f96d517b4cf5b21b7786f8b26b275cb0be
nightnotes <nightnotesplaylist@gmail.com>
2025-08-15 16:51:11 +0200

    Add files via upload

--- public/streams-append.js
+++ public/streams-append.js
@@ -1,30 +1,37 @@
 
 (function(){
   var extra = [{"name": "Dreamflow", "spotifyId": "3JxvfjZaLHM60yYRt7BYZm", "spotifyArtistUrl": "https://artists.spotify.com/c/artist/3JxvfjZaLHM60yYRt7BYZm/home"}, {"name": "Poluz", "spotifyId": "0vaXEuhH3eaJuTdMoLFdbN", "spotifyArtistUrl": "https://artists.spotify.com/c/artist/0vaXEuhH3eaJuTdMoLFdbN/home"}, {"name": "Doris Lost", "spotifyId": "43U1R9AZoGI3V5iaW6lht8", "spotifyArtistUrl": "https://artists.spotify.com/c/artist/43U1R9AZoGI3V5iaW6lht8/home"}, {"name": "Eternal", "spotifyId": "4oOqA7kbwce90hbDDKjoID", "spotifyArtistUrl": "https://artists.spotify.com/c/artist/4oOqA7kbwce90hbDDKjoID/home"}, {"name": "Slaapmutsje", "spotifyId": "1iH0DmClTXD3DEXO490gbq", "spotifyArtistUrl": "https://artists.spotify.com/c/artist/1iH0DmClTXD3DEXO490gbq/home"}, {"name": "ZizZa", "spotifyId": "20ajFDuyJzM8xGkWL9agiV", "spotifyArtistUrl": "https://artists.spotify.com/c/artist/20ajFDuyJzM8xGkWL9agiV/home"}, {"name": "Sleepy Teas", "spotifyId": "3Ax9FlTyHNJdOhAKaxhZl9", "spotifyArtistUrl": "https://artists.spotify.com/c/artist/3Ax9FlTyHNJdOhAKaxhZl9/home"}];
+  function ensureKey(obj, fromA, toB){
+    if (obj[toB] == null && obj[fromA] != null) obj[toB] = obj[fromA];
+  }
+  function normalize(a){
+    ensureKey(a, 'id', 'spotifyId');
+    return a;
+  }
   function dedupePush(arr, obj){
     if (!Array.isArray(arr)) return;
-    var exists = arr.some(function(a){ return (a.spotifyId||a.id) === (obj.spotifyId||obj.id) || a.name === obj.name; });
-    if (!exists) arr.push(obj);
+    var o = normalize(obj);
+    var exists = arr.some(function(a){
+      a = normalize(a||{});
+      return (a.spotifyId && o.spotifyId && a.spotifyId === o.spotifyId) || (a.name === o.name);
+    });
+    if (!exists) arr.push(o);
   }
   function tryAppend(){
+    var lists = [];
     if (typeof window !== 'undefined'){
-      var lists = [window.ARTISTS, window.artists, window.StreamsArtists];
-      for (var i=0;i<lists.length;i++){
-        if (Array.isArray(lists[i])){
-          for (var j=0;j<extra.length;j++){ dedupePush(lists[i], extra[j]); }
-          return true;
-        }
-      }
-      // Some apps expose ARTISTS as a const in module scope; attempt a global fallback
-      if (Array.isArray(window?.__ARTISTS__)) {
-        for (var k=0;k<extra.length;k++){ dedupePush(window.__ARTISTS__, extra[k]); }
-        return true;
+      lists = [window.ARTISTS, window.artists, window.StreamsArtists, window.__ARTISTS__];
+    }
+    var attached = false;
+    for (var i=0;i<lists.length;i++){
+      if (Array.isArray(lists[i])){
+        for (var j=0;j<extra.length;j++){ dedupePush(lists[i], extra[j]); }
+        attached = true;
       }
     }
-    return false;
+    return attached;
   }
   if (!tryAppend()){
-    // retry once after load
     window.addEventListener('load', tryAppend);
   }
-})();
+})();    
